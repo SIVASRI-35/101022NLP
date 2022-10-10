1967aa206898a5113fb7df6d7f26fede48da40b0
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2022-10-10 16:46:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['GET','POST'])
+def predict():
+    try:
+        input = request.form.get('inp')
+        model = pickle.load(open('t5model.pkl','rb'))
+        res = model.predict(input)
+        res1=''
+        for i in res:
+            res1=res1+i
+            return render_template("res.html",res1=res1,input=input)
+    except:
+        return 'error'        
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
