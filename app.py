e907b78b349b7c7962cb496dd864e04cd2def5b2
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2022-10-12 12:12:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pickle
 
 app = Flask(__name__)
 
-model = pickle.load(open('t5model.pkl','rb'))
-
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -13,6 +11,7 @@ def home():
 def predict():
     try:
         input = request.form.get('inp')
+        model = pickle.load(open('t5model.pkl','rb'))
         res = model.predict(input)
         res1=''
         for i in res:
