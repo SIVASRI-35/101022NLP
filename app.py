2d32a3d79694b6cc7f4c7622e625e3bd0dbea5c9
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2022-10-12 11:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,14 @@ def home():
 
 @app.route("/predict",methods=['GET','POST'])
 def predict():
-    try:
-        input = request.form.get('inp')
-        model = pickle.load(open('t5model.pkl','rb'))
-        res = model.predict(input)
-        res1=''
-        for i in res:
-            res1=res1+i
-            return render_template("res.html",res1=res1,input=input)
-    except:
-        return 'error'        
+    input = request.form.get('inp')
+    model = pickle.load(open('t5model.pkl','rb'))
+    res = model.predict(input)
+    res1=''
+    for i in res:
+        res1=res1+i
+        return render_template("res.html",res1=res1,input=input)
+      
 
 if __name__ == "__main__":
     app.run(debug=True)
